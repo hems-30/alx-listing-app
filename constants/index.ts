303500b4cfa097fe79117251abc0608e45d7f9a4
Hemen Solomon <hemensolomon83@gmail.com>
2025-07-06 19:25:16 +0300

    Complete milestone 1: listing page setup

---
+++ constants/index.ts
@@ -0,0 +1,10 @@
+import { Listing } from '../interfaces';
+
+export const listings: Listing[] = [
+  {
+    id: 1,
+    title: 'Modern Apartment',
+    description: 'A stylish apartment in the city center.',
+    image: '/assets/banner.jpg',
+  },
+];
